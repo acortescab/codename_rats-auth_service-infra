b82171986f97b1b8ddff98da088d3d3d7551faaf
acortescab <acortescab@gmail.com>
2026-09-25 13:39:44 +0200

    fix version

--- providers.tf
+++ providers.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0, < 6.66.0"
-    }
-  }
-}
-
+// Provider configuration for AWS resources.
 provider "aws" {
   region = var.aws_region
 }
